20313aaac69aa31b2015b7f6724626c051003da0
Nikimad <PybePoiD@gmail.com>
2023-02-26 00:17:18 +0300

    Add App component

---
+++ src/components/App/App.jsx
@@ -0,0 +1,12 @@
+import Wrapper from "../Wrapper";
+import Header from "../Header";
+import Aside from "../Aside/Aside";
+
+const App = () => (
+  <Wrapper>
+    <Aside />
+    <Header />
+  </Wrapper>
+);
+
+export default App;
